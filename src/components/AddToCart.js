d3ddb2d57ea741e459e35368fb3eec48577c1c15
Annette Adhiambo <annetaliya@gmail.com>
2025-09-25 12:47:39 -0700

    adding items to the cart

---
+++ src/components/AddToCart.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+
+function AddToCart () {
+    const [cartItem, setCartItem] = useState([])
+    const itemList = ['bread', 'butter', 'eggs', 'milk','chocos'];
+
+    function handleAddToCart(element) {
+        setCartItem([...cartItem, element ])
+        console.log(cartItem)
+       
+    }
+   
+    return (
+        <div>
+            <h1>Add to cart</h1>
+            <ul>
+                {itemList.map((item, index) => (
+                    <div key={index}>
+                        <li>{item}</li>
+                        <button onClick={() => handleAddToCart(item)}>Add to cart</button>
+                    </div>
+                ))}
+            </ul>
+            {cartItem &&
+                <div> 
+                    <h2>Cart</h2>
+                    <ul>
+                        {cartItem.length > 0 ? 
+                            cartItem.map((el, index) => (
+                                <div key={index}>
+                                    <li>{el}</li>
+                                </div>
+                            ))
+                        : ''
+                        }
+                    </ul>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default AddToCart;
